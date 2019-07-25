0530d0cffdd000bf6d5c91b9ecfe423ff7ad3d6e
SamNormcoreWayne <zcvszzy@gmail.com>
2019-07-25 14:53:10 +0800

    Deleted some funcs in XGBooststock.py

--- XGBoost_stock.py
+++ XGBoost_stock.py
@@ -21,24 +21,26 @@ class close_xgboost():
         self.filename = filename
         self.feature_cls = close(self.filename)
         self.params = {
-            """
-                Parameters here.
-            """
+            'booster': 'gbtress',
+            'objective': 'multi:softmax',
+            'gamma': 0.3,
+            'max_depth': 3,
+            'alpha': 1e-05,
+            'subsample': 0.8,
+            'colsample_bytree': 0.8,
+            'min_child_weight': 1,
+            'silent': 1,
+            'eta': 0.01,
+            'seed': 36,
+            'nthread': 4,
+            'num_class': 2,
+            'scale_pos_weight': 1,
+            'n_estimators': 500
         }
         self.low_freq_data = 0
         self.high_freq_data_1 = 0
         self.high_freq_data_2 = 0
         # self.Row_train, self.Col_train, self.Row_valid, self.Col_valid, self.Row_test, self.Col_test, self.features = self.feature_cls.func_data_split()
-
-    def wavelet(self):
-        """
-            @varName: coeff
-            @varType: list<numpy.ndarray>[3]
-        """
-        coeff = self.feature_cls.func_wavelet()
-        self.low_freq_data = coeff[0]
-        self.high_freq_data_1 = coeff[1]
-        self.high_freq_data_2 = coeff[2]
 
     def roc_auc_score(self, x, y):
         return metrics.roc_auc_score(x, y)
@@ -96,45 +98,6 @@ class close_xgboost():
         d_test = data[train_size + cv_size : ]
         return [d_train, d_cv, d_test]
 
-    
-    def train_model_with_wavelet(self):
-        num_round = 7000
-        low_freq_set = self.split_data(self.low_freq_data)
-        high_freq_1_set = self.split_data(self.high_freq_data_1)
-        high_freq_2_set = self.split_data(self.high_freq_data_2)
-
-        low_freq_data_train = low_freq_set[0]
-        low_freq_data_cv = low_freq_set[1]
-        low_freq_data_test = low_freq_set[2]
-        '''
-        high_freq_data_1_train = high_freq_1_set[0]
-        high_freq_data_1_cv = high_freq_1_set[1]
-        high_freq_data_1_test = high_freq_1_set[2]
-
-        high_freq_data_2_train = high_freq_2_set[0]
-        high_freq_data_2_cv = high_freq_2_set[1]
-        high_freq_data_2_test = high_freq_2_set[2]
-        '''
-        
-        low_freq_data_train = xgb.DMatrix(low_freq_data_train, label=['test'])
-        low_freq_data_test = xgb.DMatrix(low_freq_data_test, label=['test2'])
-        low_freq_data_cv = xgb.DMatrix(low_freq_data_cv, label=['test3'])
-        
-        d_low_watchlst = [(low_freq_data_train, 'low_train'), (low_freq_data_cv, 'low_cv')]
-        low_model = xgb.train(self.params, low_freq_data_train, num_boost_round=num_round, evals=d_low_watchlst)
-
-        preds = low_model.predict(low_freq_data_test, ntree_limit=low_model.best_ntree_limit)
-        res = pd.DataFrame(preds, columns=['pred'])
-        res.to_csv(os.path.join(os.getcwd(), "res.csv"), index=False)
-
-        train_predictions = [round(value) for value in preds]
-        train_accuracy = accuracy_score(low_freq_data_test, train_predictions)
-        print(" Accuracy: {:.2f}".format(train_accuracy * 100))
-
-        score = metrics.roc_auc_score(low_freq_data_test, preds)
-        print("score:", score)
-        plot_importance(low_model, max_num_features=-20)
-        plt.show()
     """
     def create_feature_map(self):
         with open(os.path.join(os.getcwd(), "xgb.fmap", 'w')) as fp:
@@ -157,10 +120,8 @@ class close_xgboost():
 
 def main():
     model = close_xgboost("SPY.csv")
-    model.wavelet()
     #model.train_model_origin_interface()
     #model.create_feature_map()
-    model.train_model_with_wavelet()
 
 
 if __name__ == '__main__':
